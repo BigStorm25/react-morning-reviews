da02c5f2cf6537609d4484f37b01cea08951b11e
BigStorm <lucasstormmartin@gmail.com>
2020-07-22 11:35:50 -0500

    First morning review

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import List from './components/List'
+import './App.css'
 
+
+const App = () => {
+    return (
+        <div className='App'>
+            <h1 className='movie-list-title'>The most amazing movie list</h1>
+            <List />
+        </div>
+    )
+}
+
+export default App
